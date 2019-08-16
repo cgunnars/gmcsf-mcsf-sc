17afb8b1bfa9c28ead1071fe3d5bba2cb05916c0
cgunnars <gunnars@mit.edu>
2019-08-16 16:15:38 -0400

    plotting

---
+++ src/plotMarkers.R
@@ -0,0 +1,23 @@
+library(argparse)
+library(Seurat)
+library(cowplot)
+library(ggplot2)
+
+parser <- ArgumentParser(description='File i/o for single cell RNAseq')
+parser$add_argument('-i', type="character", nargs=1,
+                    help='data file for input')
+parser$add_argument('-m', type='character', nargs=1,
+                    help='txt file containing list of markers to plot')
+parser$add_argument('-o', type='character', nargs=1,
+                    help='filestem for figure output')
+args <- parser$parse_args()
+mergeruns <- readRDS(args$i)
+con <- file(args$m, open="r")
+markers <- readLines(con)
+close(con)
+
+plot.stim <- VlnPlot(mergeruns, features = markers, group.by ='stim', pt.size = 0.01, combine = TRUE)
+plot.donor <- VlnPlot(mergeruns, features = markers, split.by='stim', group.by='donor', pt.size = 0, combine=TRUE)
+
+ggsave(paste(args$o, '-stim.svg', sep=''), plot=plot.stim, height = 20, width = 12)
+ggsave(paste(args$o, '-donor.svg', sep=''), plot=plot.donor, height = 20, width = 12)
